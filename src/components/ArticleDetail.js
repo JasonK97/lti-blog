3d03d8c0cad4330b520a4fc40332229f6a7fe891
JasonK97 <jasonbkent@gmail.com>
2021-11-05 09:33:01 -0600

    broadened the html serializer

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -27,11 +27,35 @@ export const htmlSerializer = function(type, element, content, children, key) {
     case Elements.heading3: // Heading 3
       return React.createElement('h3', propsWithUniqueKey(props, key), children)
 
+    case Elements.heading4: // Heading 4
+      return React.createElement('h4', propsWithUniqueKey(props, key), children)
+      
+    case Elements.heading5: // Heading 5
+      return React.createElement('h5', propsWithUniqueKey(props, key), children)
+      
+    case Elements.heading6: // Heading 6
+      return React.createElement('h6', propsWithUniqueKey(props, key), children)
+
+    case Elements.preformatted: // Preformatted
+      return React.createElement('pre', propsWithUniqueKey(props, key), children)
+      
+    case Elements.strong: // Strong
+      return React.createElement('strong', propsWithUniqueKey(props, key), children)
+      
+    case Elements.em: // Emphasis
+      return React.createElement('em', propsWithUniqueKey(props, key), children)
+
     case Elements.listItem: // Unordered List Item
       return React.createElement('li', propsWithUniqueKey(props, key), children)
       
     case Elements.oListItem: // Ordered List Item
       return React.createElement('li', propsWithUniqueKey(props, key), children)
+
+    case Elements.list: // Unordered List
+      return React.createElement('ul', propsWithUniqueKey(props, key), children)
+      
+    case Elements.oList: // Ordered List
+      return React.createElement('ol', propsWithUniqueKey(props, key), children)
 
     // Add a class to paragraph elements
     case Elements.paragraph:
