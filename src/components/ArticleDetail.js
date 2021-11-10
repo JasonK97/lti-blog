3fb13463e0570153671b36ca7f4f35eab5378dd1
JasonK97 <jasonbkent@gmail.com>
2021-11-10 15:00:48 -0700

    Began adding styled-components

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -4,6 +4,29 @@ import { useQuery } from '@apollo/client'
 import { getDetails } from '../GraphQL/Queries'
 import { RichText, Elements } from 'prismic-reactjs'
 import { Link, useParams } from 'react-router-dom'
+import styled from 'styled-components'
+
+const ArticleHeading = styled.div`text-align: center;`
+const ArticleContent = styled.div`padding: 0% 30% 3% 30%;`
+const BackButton = styled.button`
+  background-color:rgb(230, 230, 230);
+  border-radius: 5px;
+  border: none;
+  padding: 5px;
+  transition: all 0.1s ease-in-out;
+  &:hover {
+    background-color:rgb(189, 189, 189);
+    box-shadow: 3px 3px 10px 0 rgba(0, 0, 0, 0.2);
+    transform: translate3d(0px, -1px, 0px);
+  }
+`
+
+const linkStyle = {
+  textDecoration: 'none',
+  color: 'black',
+  fontFamily: 'filson-pro',
+  fontWeight: '600'
+}
 
 // Function to add a unique key to props
 const propsWithUniqueKey = function(props, key) {
@@ -47,16 +70,16 @@ function ArticleDetail() {
         alt={data.article.feature_image.alt}
         width='30%' 
       />
-      <div className='article-heading'>
+      <ArticleHeading>
         <h1>{data.article.title[0].text}</h1>
         <h4>{data.article.published_at.substring(0, 10)}</h4>
-      </div>
-      <div className='article-content'>
+      </ArticleHeading>
+      <ArticleContent>
         {data.article.body.filter(b => b.type === 'inline_text').map((content, index) => {
           return <RichText key={index} render={content.primary.description} htmlSerializer={htmlSerializer} />
         })}<br />
-        <Link className='back-btn' to='/'><strong>Go Back ...</strong></Link>
-      </div>
+        <BackButton><Link style={linkStyle} to='/'>Go Back ...</Link></BackButton>
+      </ArticleContent>
     </div>
   )
 }
--- src/components/ArticlesGrid.js
+++ src/components/ArticlesGrid.js
@@ -2,6 +2,20 @@ import React from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_POSTS } from '../GraphQL/Queries'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const ArticleTile = styled.div`
+  border: 1px solid rgb(230, 230, 230);
+  border-radius: 15px;
+  text-align: center;
+  padding: 0px 0px 15px 0px;
+  margin: 20px 30% 15px 30%;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    box-shadow: 10px 10px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    transform: translate3d(0px, -1px, 0px);
+  }
+`
 
 function Posts() {
   const { loading, error, data } = useQuery(GET_POSTS)
@@ -12,7 +26,7 @@ function Posts() {
   return (
     <div>
       {data.allArticles.edges.map((article) => (
-        <div key={article.node._meta.id} className='article-tile'>
+        <ArticleTile key={article.node._meta.id}>
           <Link className='cont-reading' to={`/${article.node._meta.uid}`}>
             <img
               className='grid-image'
@@ -20,7 +34,7 @@ function Posts() {
               alt={article.node.feature_image.alt}
               width='100%'
             />
-            <div className='text-content'>
+            <div className="text-content">
               <h1>{article.node.title[0].text}</h1>
               <h3>Published : {article.node.published_at.substring(0, 10)}</h3>
               <div>
@@ -33,7 +47,7 @@ function Posts() {
               </div>
             </div>
           </Link>
-        </div>
+        </ArticleTile>
       ))}
     </div>
   )
